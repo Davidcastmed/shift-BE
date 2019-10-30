4f543f3eb7db793652c50b4050aa9940d63d10f6
David Castillo <davidcastillo@Davids-MBP.fritz.box>
2019-10-30 13:44:57 +0100

    adding async function to the routers and also now is separated from the index to get it more organized

--- src/index.js
+++ src/index.js
@@ -1,24 +1,13 @@
 const express = require('express')
 require('./db/mongoose')
-const User = require('./db/models/users')
-
+const userRouter = require('./routers/user')
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json())
+app.use(userRouter)
 
-app.post('/users', (req, res) => {
-    const user = new User(req.body)
-    user.save().then(() => {
-        res.send(user)
-    }).catch((e) => {
-        res.status(400).send(e)
-        // res.send(e)
-
-    })
-
-})
 
 app.listen(port, () =>
     console.log(`Example app listening on port ${port}!`));
